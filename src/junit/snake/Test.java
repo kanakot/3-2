34bd9210cbaf9b8fe0077efcbcd25377ddf0ce93
g16943tk <g16943tk@gm.tsuda.ac.jp>
2018-05-10 14:23:45 +0900

    デバッグ

---
+++ src/junit/snake/Test.java
@@ -0,0 +1,22 @@
+package junit.snake;
+
+import static org.junit.Assert.*;
+
+public class Test {
+
+	@org.junit.Test
+	/*public void test() {
+		fail("Not yet implemented");
+	}*/
+	public void snakeToCamelcaseで入力したスネークケースの文字列を取得する(){
+		StringBuilder sb = new StringBuilder();
+		assertNotNull(sb);
+		
+	}
+	
+	public void camelToSnakecaseで入力したスネークケースの文字列を取得する(){
+		StringBuilder sb = new StringBuilder();
+		assertNotNull(sb);
+	}
+
+}
